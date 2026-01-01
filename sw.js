58446c62972065ad2c4d0388374aef2551f0eec4
cmstudios12 <153551908+cmstudios12@users.noreply.github.com>
2026-01-01 16:36:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "satech-cache-v1";
+const cacheName = "satech-cache-v2";
 const assets = [
   "/",                
   "/index.html",      
@@ -25,3 +25,4 @@ self.addEventListener("fetch", event => {
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
